43896c27a75be258b5970bddaaab3f723ad86e7c
Logedbog <68834352+Logedbog@users.noreply.github.com>
2024-03-17 21:21:09 -0400

    Update GhostWareLib.lua

--- GhostWareLib.lua
+++ GhostWareLib.lua
@@ -1859,7 +1859,7 @@ do
             Size = UDim2.new(0, 216, 1, 0);
             Position = UDim2.new(1, 6, 0, 0);
             TextSize = 14;
-            RichText = true
+            RichText = true;
             Text = Info.Text;
             TextXAlignment = Enum.TextXAlignment.Left;
             ZIndex = 6;
